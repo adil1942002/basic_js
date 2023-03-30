6eeef3b430defe8e4331918c1e2a7da0b2927acf
adil <adil1942002@gmail.com>
2023-03-30 10:51:21 -0700

    upload
    project upload

---
+++ app.js
@@ -0,0 +1,79 @@
+alert("Hello Adil");
+
+
+window.alert("ok");
+
+
+var fname = "Ubaid";
+fname = "Adil";
+console.log(fname);
+
+
+var define;
+define = "use";
+console.log(define);
+
+var num1 = 150;
+var newnum = num1 + 150;
+console.log(newnum);
+
+
+var num1 = 150;
+alert(num1);
+
+var num2 = 10 + 10;
+console.log(num2);
+
+var num2 = 10 - 5;
+console.log(num2);
+
+var num3 = 100;
+var num4 = 50;
+var num5 = num3 + num4;
+console.log(num5);
+
+var num6 = 10;
+var num7 = 10;
+var num8 = num6 * num7;
+console.log(num8);
+
+var num9  = 10 % 3;
+console.log(num9);
+
+var num10 = 150;
+console.log(++num10);
+
+var num11 = 150;
+var num12 =150;
+var num13 =num11 + num12;
+var num14 = ++num13 +500;
+console.log(num14);
+ var num15 = (2 + 2) * 10 / 2;
+ console.log(num15);
+
+var num16 = 2 + (2 * 10) - 2;
+console.log(num16);
+
+
+var fName ="Adil";
+var lName =" Raza ";
+var city = "Karachi";
+var fullnName = fName + lName + " For " + city;
+console.log(fullnName);
+
+var fName = prompt("Enter Your First Name","Adil");
+var lName =prompt("Enter Your Last Name"," Raza ");
+var city =prompt("Enter Your City Name","Karachi");
+var fullName = fName + lName + " For " + city;
+console.log(fullName);
+
+
+
+var yourName ="Adil";
+var firstName = prompt("Enter Your First Name");
+if(yourName === firstName){
+    console.log("Hello Adil !");
+}
+else{
+    console.log("Not Correct");
+}
